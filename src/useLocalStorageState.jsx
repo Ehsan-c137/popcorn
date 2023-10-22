aeaed692dbc2818e2dfea0d8c8081bd7b09f7790
ehsan-c137 <ehsanmousavi1.79@gmali.com>
2023-10-22 11:37:31 +0330

    initial commit

---
+++ src/useLocalStorageState.jsx
@@ -0,0 +1,15 @@
+import { useEffect } from "react";
+import { useState } from "react";
+
+export function useLocalStorageState(initialState, key) {
+   const [value, setValue] = useState(function () {
+      const storedValue = localStorage.getItem(key);
+      return storedValue ? JSON.parse(storedValue) : [];
+   });
+
+   useEffect(() => {
+      localStorage.setItem(key, JSON.stringify(value));
+   }, [value, key]);
+
+   return [value, setValue];
+}
